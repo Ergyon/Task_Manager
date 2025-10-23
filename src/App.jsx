787172b2efd752f7ea83754165de678d89953db0
Ergyon <benoitdoudard56@gmail.com>
2025-10-24 00:20:05 +0200

    feat(AppContent)

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
-import "./App.css"
+
 import Header from "./components/Header/Header.jsx"
-import { ThemeProvider } from "./contexts/ThemeContext"
+import { TaskProvider } from "./contexts/TaskContext.jsx"
+import { ThemeProvider, useTheme } from "./contexts/ThemeContext"
 import Agenda from "./pages/Agenda.jsx"
 import HomePage from "./pages/HomePage.jsx"
 import Stats from "./pages/Stats.jsx"
@@ -10,14 +11,27 @@ function App() {
   return (
     <ThemeProvider>
       <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/agenda" element={<Agenda />} />
-          <Route path="/stats" element={<Stats />} />
-        </Routes>
+        <TaskProvider>
+          <AppContent />
+        </TaskProvider>
       </BrowserRouter>
     </ThemeProvider>
   )
 }
+
+const AppContent = () => {
+  const { theme } = useTheme()
+
+  return (
+    <div className={theme}>
+      <Header />
+      <Routes>
+        <Route path="/" element={<HomePage />}></Route>
+        <Route path="/agenda" element={<Agenda />}></Route>
+        <Route path="/stats" element={<Stats />}></Route>
+      </Routes>
+    </div>
+  )
+}
+
 export default App
