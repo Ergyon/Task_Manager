c0b8898f6ed7a7ba83c9ab2b21ad1e8d5d4cb5f6
Ergyon <benoitdoudard56@gmail.com>
2025-10-24 00:21:53 +0200

    feat(taskForm); add a new task

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,62 @@
+import { useState } from "react"
+import { useTasks } from "../contexts/TaskContext"
+
 const HomePage = () => {
+  const [taskName, setTaskname] = useState("")
+  const { tasks, addTask } = useTasks()
+
+  // creer un id
+  const generateId = () => {
+    return `${Date.now()}-${Math.random().toString(36).substr(2, 9)}`
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    if (taskName.trim() === "") {
+      alert("Entrez un nom")
+      return
+    }
+
+    const newTask = {
+      id: generateId(),
+      name: taskName,
+      initDate: new Date().toISOString(),
+      limitDate: null,
+      importance: null,
+      category: null,
+      isComplete: false
+    }
+
+    addTask(newTask)
+    setTaskname("")
+  }
+
   return (
-    <div>
-      <h2>Page des taches</h2>
+    <div className="home-container">
+      <h2>Mes Tâches</h2>
+
+      <form className="taskform" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Nouvelle tâche"
+          value={taskName}
+          onChange={(e) => setTaskname(e.target.value)}
+        />
+        <button className="taskform-addBtn" type="submit">
+          Ajouter
+        </button>
+      </form>
+
+      <div className="tasklist-container">
+        <h3>Listes des taches ({tasks.length})</h3>
+        {tasks.map((task) => (
+          <div key={task.id}>
+            <p>{task.name}</p>
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
-
 export default HomePage
