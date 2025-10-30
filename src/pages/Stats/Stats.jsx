339179ba192cb5f22c8169ce704766aa0fb3ab45
Ergyon <benoitdoudard56@gmail.com>
2025-10-30 02:19:13 +0100

    Created Stats page

--- src/pages/Stats/Stats.jsx
+++ src/pages/Stats/Stats.jsx
@@ -1,7 +1,16 @@
+import TaskNav from "../../components/TasksNav/TaskNav"
+import { useTasks } from "../../contexts/TaskContext"
+import { useTasksFilter } from "../../utils/taskFilter"
+import "./Stats.css"
+
 const Stats = () => {
+  const { tasks } = useTasks()
+  const { activeFilter, setActiveFilter } = useTasksFilter(tasks)
+
   return (
-    <div>
-      <h2>Statistiques</h2>
+    <div className="stats-container">
+      <h2 className="stats-title">Statistiques</h2>
+      <TaskNav activeFilter={activeFilter} onFilterChange={setActiveFilter} />
     </div>
   )
 }
